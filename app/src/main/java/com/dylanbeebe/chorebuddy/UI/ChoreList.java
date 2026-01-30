04ac46e2d2aa008a79ed71aa4e6bb8a211f7972c
Dylan <dylbeebe@gmail.com>
2026-01-30 17:55:48 -0600

    Add Chore List activity
    - Add complete ChoreList.java wireframe
    - Add buttons: report, new chore
    - Add searchbar

--- app/src/main/java/com/dylanbeebe/chorebuddy/UI/ChoreList.java
+++ app/src/main/java/com/dylanbeebe/chorebuddy/UI/ChoreList.java
@@ -1,4 +1,43 @@
 package com.dylanbeebe.chorebuddy.UI;
 
-public class ChoreList {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.dylanbeebe.chorebuddy.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class ChoreList extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setEdgeToEdgeContentView(R.layout.activity_chore_list);
+
+        FloatingActionButton fab = findViewById(R.id.choreList_new);
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChoreList.this, ChoreDetails.class);
+                startActivity(intent);
+            }
+
+        });
+
+        FloatingActionButton reportFab = findViewById(R.id.choreList_report);
+
+    }
+
+    void report() {
+        // start share intent with generated pdf
+
+        // get sorted current and completed chores from repository and generate a tabular csv report then convert to pdf and return
+
+
+    }
+
+
 }
