65762ca21c65d530b7c077f311c808db55ad4cdd
Dylan <dylbeebe@gmail.com>
2026-01-31 12:32:59 -0600

    Add Chore dao

--- app/src/main/java/com/dylanbeebe/chorebuddy/dao/ChoreDAO.java
+++ app/src/main/java/com/dylanbeebe/chorebuddy/dao/ChoreDAO.java
@@ -1,4 +1,28 @@
 package com.dylanbeebe.chorebuddy.dao;
 
-public class ChoreDAO {
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.dylanbeebe.chorebuddy.entities.Chore;
+
+import java.util.List;
+
+@Dao
+public interface ChoreDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    long insert(Chore chore);
+
+    @Update
+    void update(Chore chore);
+
+    @Delete
+    void delete(Chore chore);
+
+    @Query("SELECT * FROM chores ORDER BY id ASC;")
+    List<Chore> getAllChores();
+
 }
