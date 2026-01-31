3009127b87c86978e434e11bb720f786d4e680df
Dylan <dylbeebe@gmail.com>
2026-01-30 19:50:41 -0600

    Add Chore Details activity
    - Add complete ChoreDetails.java wireframe
    - Add details form inputs
    - Add buttons: delete, save chore

---
+++ app/src/main/java/com/dylanbeebe/chorebuddy/UI/ChoreDetails.java
@@ -0,0 +1,15 @@
+package com.dylanbeebe.chorebuddy.UI;
+
+import android.os.Bundle;
+
+import com.dylanbeebe.chorebuddy.R;
+
+public class ChoreDetails extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setEdgeToEdgeContentView(R.layout.activity_chore_details);
+
+    }
+}
