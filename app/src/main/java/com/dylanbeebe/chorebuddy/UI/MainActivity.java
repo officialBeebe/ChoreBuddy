4a37a4251350be30bcf45759951771989cb79e4a
Dylan <dylbeebe@gmail.com>
2026-01-31 18:34:47 -0600

    Finish activity after start ChoreList

--- app/src/main/java/com/dylanbeebe/chorebuddy/UI/MainActivity.java
+++ app/src/main/java/com/dylanbeebe/chorebuddy/UI/MainActivity.java
@@ -3,12 +3,6 @@ package com.dylanbeebe.chorebuddy.UI;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import com.dylanbeebe.chorebuddy.R;
 
 public class MainActivity extends BaseActivity {
@@ -19,16 +13,9 @@ public class MainActivity extends BaseActivity {
 
         setEdgeToEdgeContentView(R.layout.activity_main);
 
-
-        // countdown 3, 2, 1, then start ChoreList activity
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-
         Intent intent = new Intent(MainActivity.this, ChoreList.class);
         startActivity(intent);
+        finish();
     }
 }
 
